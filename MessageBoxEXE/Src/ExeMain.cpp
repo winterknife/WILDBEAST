aac685fd53a6cb68d514bcb4d0b949c83192b9c2
winterknife <winterknife@protonmail.com>
2025-08-09 14:18:36 -0400

    added custom printf function to EXE project

--- MessageBoxEXE/Src/ExeMain.cpp
+++ MessageBoxEXE/Src/ExeMain.cpp
@@ -1,21 +1,33 @@
 #include <Windows.h>
+#include <stdio.h>
 #include "Load.h"
 #include "Resource.h"
+
+extern "C" __attribute__((noinline)) void __stdcall log_console(_Printf_format_string_ PCSTR strFormat, ...) {
+    // Init local variables
+    va_list pchArgs;
+
+    // Printf message
+    va_start(pchArgs, strFormat);
+    vprintf_s(strFormat, pchArgs);
+    va_end(pchArgs);
+}
 
 extern "C" __attribute__((noreturn)) VOID __stdcall ExeEntry(
 	VOID
 ) {
 	DWORD dwSize = 0;
 	PVOID pData = NULL;
-	HANDLE hStdOutput = NULL;
 
 	extract_payload(GetModuleHandle(NULL), IDR_PAYLOAD, RCDATA, &dwSize, &pData);
 
 	AllocConsole();
 
-	hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	AttachConsole(ATTACH_PARENT_PROCESS);
 
-	WriteConsoleA(hStdOutput, pData, (DWORD)strlen((PCSTR)pData), NULL, NULL);
+	freopen("CONOUT$", "wb", stdout);
+
+	log_console("%s\n", pData);
 
 	MessageBoxA(NULL, (PCSTR)pData, "TEST!", MB_OK);
 
